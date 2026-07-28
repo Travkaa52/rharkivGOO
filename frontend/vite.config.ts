f25a952b4f1e93f5b9ca7a7c9d8414cab2185e07
Travkaa52 <dimakasan028@gmail.com>
2026-07-28 19:47:11 +0300

    Update maximum file size to cache in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
         ]
       },
       workbox: {
-        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024, // <--- Додайте цей рядок сюди
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
@@ -49,6 +49,8 @@ export default defineConfig({
           }
         ]
       }
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
