6752f123151a1e7dcd3a4678c8836a52df392e3e
Travkaa52 <dimakasan028@gmail.com>
2026-07-30 16:01:59 +0300

    Import design system CSS in main.tsx
    Added import for design system CSS.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+import './design-system.css';
 import App from '@/App';
 import { initTelegramApp } from '@/lib/telegram';
 import { useAuthStore } from '@/store/useAuthStore';
